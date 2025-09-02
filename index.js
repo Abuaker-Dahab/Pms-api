ac405022041b2e6f79da21d6186dd12c51fc51a9
abubaker-dahab <abubakerdahab@outlook.com>
2025-09-02 13:19:12 +0200

    add user auth route and update the user model

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 require("dotenv").config();
-const express = require("express");
+const express = require("express")
+const mongoose = require("mongoose");
 
 const app = express();
+const db = process.env.DB;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 })
 
-const port = process.env.PORT || 4000;
+mongoose
+ .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+    .then(() => console.log(`Connected to DB...`));
+
 const server = app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
 );
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 
 const userSchema = new mongoose.Schema(
   {
@@ -35,4 +38,54 @@ const userSchema = new mongoose.Schema(
 );
 
 
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      role: this.role,
+    },
+    process.env.JWT_PRIVATE_KEY,
+    { expiresIn: process.env.JWT_EXPIRE }
+  );
+  return token;
+};
+
+function validateRegister(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(4).max(10).required(),
+    email: Joi.string().min(8).max(255).required().email(),
+    password: Joi.string().min(6).max(255).required(),
+  });
+
+  return schema.validate(user);
+}
+
+function validateLogin(user) {
+  const schema = Joi.object({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6).max(255),
+  });
+  return schema.validate(user);
+}
+
+function validateUser(user) {
+  const schema = Joi.object({
+    profileImage: Joi.string().allow(""),
+    name: Joi.string().min(4).max(10),
+    password: Joi.string().min(6).max(255),
+    role: Joi.string(),
+  });
+
+  return schema.validate(user);
+}
+
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
 module.exports.User = mongoose.model("User", userSchema);
+module.exports.validateRegister = validateRegister;
+module.exports.validateUser = validateUser;
+module.exports.validateLogin = validateLogin;
