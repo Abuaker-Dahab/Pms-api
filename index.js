f598d46856e952f9f19ec2328db4bbab8a4d717d
abubaker-dahab <abubakerdahab@outlook.com>
2025-09-02 12:52:14 +0200

    run the server with simple route hello world!

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+})
+
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () =>
+  console.log(`Listening on port ${port}...`)
+);
+
+module.exports = server;
